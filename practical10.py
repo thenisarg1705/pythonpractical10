1578ef576c5b05836d769ab31dbc13240754246d
thenisarg1705 <97789612+thenisarg1705@users.noreply.github.com>
2022-04-13 19:07:08 +0530

    Create practical10.py

---
+++ practical10.py
@@ -0,0 +1,33 @@
+import img2pdf
+
+from PIL import Image
+
+import os
+
+img_path = "result.png"
+
+pdf_path = "result.pdf"
+
+# opening image
+
+image = Image.open(img_path)
+
+# converting into chunks using img2pdf
+
+pdf_bytes = img2pdf.convert(image.filename)
+
+# opening or creating pdf file
+
+file = open(pdf_path, "wb")
+
+# writing pdf files with chunks
+
+file.write(pdf_bytes)
+
+# closing image file
+
+image.close()
+
+# closing pdf file
+
+file.close()
